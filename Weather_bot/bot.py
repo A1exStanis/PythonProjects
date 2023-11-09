cc72d9e98a23700662bd8f348077f584bc05fd42
Alex_Stanis <alexstanis11@gmail.com>
2023-11-09 19:49:49 +0200

    +

--- Weather_bot/bot.py
+++ Weather_bot/bot.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot.types import Message
 import constant, json, requests
-
+import utills
 
 bot = telebot.TeleBot(constant.API_KEY)
 
@@ -12,6 +12,7 @@ def start(message:Message) -> None:
 
 @bot.message_handler(content_types=['text'])
 def get_weather(message: Message) -> None:
+    markup = utills.build_inline_markup()
     city = message.text.strip().lower()
     result = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={constant.API_weather}&units=metric')
     data = json.loads(result.text)
