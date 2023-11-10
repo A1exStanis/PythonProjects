f2086d2f951d18175d33a6aac2b771dc042344b9
Alex_Stanis <alexstanis11@gmail.com>
2023-11-10 23:41:58 +0200

    +

--- Weather_bot/bot.py
+++ Weather_bot/bot.py
@@ -12,10 +12,29 @@ def start(message:Message) -> None:
 
 @bot.message_handler(content_types=['text'])
 def get_weather(message: Message) -> None:
-    markup = utills.build_inline_markup()
+    markup = utills.build_reply_markup(constant.MAIN_SCREEN_BUTTONS)
     city = message.text.strip().lower()
     result = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={constant.API_weather}&units=metric')
     data = json.loads(result.text)
-    bot.send_message(message.chat.id, data['main']['temp'])
+    bot.send_message(message.chat.id, 
+                     constant.GUIDE,
+                     reply_markup=markup)
+    bot.register_next_step_handler(message, message_varification_handler, data)
+
+def message_varification_handler(message: Message, data: dict) -> None:
+    if message.text == constant.BACK:
+        bot.send_message(message.chat.id, constant.HI_COMAND)
+    elif message.text == constant.TEMPERATURE:
+        markup = utills.build_reply_markup(constant.MAIN_SCREEN_BUTTONS)
+        bot.send_message(message.chat.id, 
+                 data['main']['temp'],
+                 reply_markup=markup)
+        bot.register_next_step_handler(message,message_varification_handler, data)
+    elif message.text == constant.WHEATHER:
+        pass
+    elif message.text == constant.WIND:
+        pass
+    elif message.text == constant.SUNRISE_SET:
+        pass
 
 bot.polling(non_stop=True)
--- Weather_bot/constant.py
+++ Weather_bot/constant.py
@@ -1,5 +1,20 @@
 API_KEY = '6828567541:AAGWU7-fd4mDrzh980U3jujTKMyfmTVIEWo'
 API_weather = '6a2e96dbdcbe64768b7746f42e9f3aba'
-HI_COMAND = 'Введіть будь ласка місто: '
+HI_COMAND = 'Please, enter your city : '
+
+BACK = 'Back'
+
+TEMPERATURE = 'Temperature'
+WHEATHER = 'Wheather'
+WIND = 'Wind'
+SUNRISE_SET = 'Sunrise/Sunset'
+
+MAIN_SCREEN_BUTTONS = (TEMPERATURE,
+                       WHEATHER,
+                       WIND,
+                       SUNRISE_SET, 
+                       BACK,)
+
+GUIDE = 'Choose what you want to know: '
 
 # https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=6a2e96dbdcbe64768b7746f42e9f3aba
