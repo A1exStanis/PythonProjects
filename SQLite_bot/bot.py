bae2ff38c8cd1f11260db2187081b5fed76731f2
Alex_Stanis <alexstanis11@gmail.com>
2023-11-17 21:55:52 +0200

    + 2 func

--- SQLite_bot/bot.py
+++ SQLite_bot/bot.py
@@ -1,6 +1,7 @@
 import telebot
 import sqlite3
 import constant
+import datetime as dt
 
 bot = telebot.TeleBot(constant.API_KEY)
 
@@ -14,7 +15,18 @@ def start(message) -> None:
     connect.commit()
     typer.close()
     connect.close()
-
+    name = get_name(message)
+    time = get_time(message)
+    print(time)
     bot.send_message(message.chat.id, 'Done')
+
+
+def get_name(message) -> str:
+    name = message.from_user.first_name + " " + message.from_user.last_name
+    return name
+
+def get_time(message) -> str:
+    time = dt.datetime.fromtimestamp(message.date)
+    return time
 
 bot.polling(none_stop=True)
