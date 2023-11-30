4e9878beee80ae8a5b5af944bda88e55bb191a4c
Alex_Stanis <alexstanis11@gmail.com>
2023-11-30 21:14:32 +0200

    +

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    sum = 0
+    digit_1 = 1
+    digit_2 = 0
+    for i in range(n):
+        sum = digit_2 + digit_1
+        digit_2 = digit_1 + digit_2
+        digit_1 = sum - digit_1
+
+    print(sum)
+
+
+fibonacci(10)
