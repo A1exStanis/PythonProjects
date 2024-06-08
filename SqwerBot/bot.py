ff41d2054c472502a0aa4e6a5fc6ead7361df610
Alex_Stanis <alexstanis11@gmail.com>
2024-06-08 23:55:15 +0300

    +1

--- SqwerBot/bot.py
+++ SqwerBot/bot.py
@@ -2,10 +2,18 @@ import telebot
 from telebot.types import Message
 from utils import (
     build_reply_markup,
-    build_inline_markup,
     phone_request
 )
 import constant
+import pandas as pd
+
+
+def receive_df_from_sheet() -> pd.DataFrame:
+    return pd.read_csv(
+        constant.SPREADSHEETS_1.format(
+            SHEET_ID=constant.SHEET_ID,)
+    )
+
 
 bot = telebot.TeleBot(constant.API_KEY)
 
@@ -21,6 +29,7 @@ def start(message: Message) -> None:
 
 @bot.message_handler(content_types=['text'])
 def next_step(message: Message) -> None:
+    care_df = receive_df_from_sheet()
     if message.text == constant.FEEDBACK:
         markup = phone_request()
         bot.send_message(message.chat.id,
@@ -28,17 +37,34 @@ def next_step(message: Message) -> None:
                          reply_markup=markup)
         bot.register_next_step_handler(message, receive_call_handler)
     elif message.text == constant.ORDER_3_7_DAYS:
-        pass
+        bot.send_message(message.chat.id, message.chat.id)
     elif message.text == constant.ORDER_12_15_DAYS:
         pass
     elif message.text == constant.SPECIAL_OFFERS:
         pass
+    elif message.text == constant.ABOUT_US:
+        bot.send_message(
+            message.chat.id,
+            care_df.loc[constant.SPREADSHEET_LOC_BY_TEXT[constant.SNEAKERS]],
+        )
+    elif message.text == constant.MY_GOODS:
+        markup = phone_request()
+        bot.send_message(message.chat.id,
+                         constant.MY_GOODS_MESSAGE,
+                         reply_markup=markup)
+        bot.register_next_step_handler(message, receive_call_handler)
+    else:
+        markup = markup = build_reply_markup(constant.WELCOME_BUTTONS)
+        bot.send_message(message.chat.id,
+                         constant.USE_BUTTONS_MESSAGE,
+                         reply_markup=markup)
+        bot.register_next_step_handler(message, next_step)
 
 
 def receive_call_handler(message: Message) -> None:
     if message.contact:
         markup = build_reply_markup([constant.BACK])
-        bot.send_contact(message.chat.id,
+        bot.send_contact(message.chat.id,    #constant.VLAD_CHAT_ID
                          phone_number=message.contact.phone_number,
                          first_name=message.contact.first_name,
                          last_name=message.contact.last_name)
